964f58b6811fc317e4aa39e829d5d46f175e4e4a
VictorSantosSilvaa <157403216+VictorSantosSilvaa@users.noreply.github.com>
2024-05-27 13:57:41 -0300

    update

---
+++ cadastro/cadastro.js
@@ -0,0 +1,35 @@
+const nameu = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+const button = document.getElementById("botao");
+
+button.addEventListener("click", function (event) {
+
+    event.preventDefault();
+
+    const userData = JSON.stringify({
+        name: nameu.value,
+        email: email.value,
+        password: password.value,
+        confirmPassword: confirmPassword.value,
+
+    });
+    console.log(userData)
+
+    fetch("http://10.92.198.38:8080/auth/signup", {
+        method: "POST",
+        body: userData,
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+        .then((result) => {
+            return result.json();
+        })
+        .then((data) => {
+            console.log(data)
+            window.location.href = "/login/index.html";
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+});
---
+++ login/login.js
@@ -0,0 +1,35 @@
+const emailLogin = document.getElementById("email");
+const passwordLogin = document.getElementById("password");
+const button1 = document.getElementById("botao");
+
+button1.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const userData1 = JSON.stringify({
+        email: emailLogin.value,
+        password: passwordLogin.value,
+    });
+
+    fetch("http://10.92.198.38:8080/auth/signin", {
+        method: "POST",
+        body: userData1,
+        headers: { "Content-type": "application/json; charset=UTF-8" }
+    })
+        .then((result) => {
+            return result.json();
+        })
+        .then((data) => {
+            // Verifica se a resposta da API contém um token
+            if (data.token) {
+                // Armazena o token no localStorage
+                localStorage.setItem('token', data.token);
+                console.log('Token armazenado com sucesso:', data.token);
+                window.location.href = "/postagem/index.html";
+            } else {
+                console.log('Token não encontrado na resposta da API');
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
